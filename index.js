a1bce57198476b7d2420379894fb26b2fb8f249d
Trần Quốc Vũ <vutq@softworld.jp>
2018-03-21 20:20:41 +0700

    update code

--- index.js
+++ index.js
@@ -76,6 +76,7 @@ function handleMessage(sender_psid, received_message) {
   } else if (received_message.attachments) {
     // Get the URL of the message attachment
     let attachment_url = received_message.attachments[0].payload.url;
+    console.log(attachment_url);
     response = {
       "attachment": {
         "type": "template",
